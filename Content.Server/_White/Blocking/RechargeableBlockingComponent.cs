859a249a8b99ed160cb2234cb59d694641962540
GreyScaleHarlequin <195893253+GreyScaleHarlequin@users.noreply.github.com>
2026-05-17 12:58:24 +0100

    Update RechargeableBlockingComponent.cs

--- Content.Server/_White/Blocking/RechargeableBlockingComponent.cs
+++ Content.Server/_White/Blocking/RechargeableBlockingComponent.cs
@@ -3,12 +3,18 @@ namespace Content.Server._White.Blocking;
 [RegisterComponent]
 public sealed partial class RechargeableBlockingComponent : Component
 {
-    [DataField, ViewVariables(VVAccess.ReadWrite)]
-    public float DischargedRechargeRate = 1.33f;
+    // Aurora's Song | As of right now none of these variables actually define or change any behaviour in the system's
+    // logic, which we may change in the future but this was the easiest way to make this work with the more updated power
+    // and battery systems we've inherited from Upstream(WizDen)
 
-    [DataField, ViewVariables(VVAccess.ReadWrite)]
-    public float ChargedRechargeRate = 2f;
+    // [DataField, ViewVariables(VVAccess.ReadWrite)]
+    // public float DischargedRechargeRate = 1.33f;
+    //
+    // [DataField, ViewVariables(VVAccess.ReadWrite)]
+    // public float ChargedRechargeRate = 2f;
 
     // [ViewVariables] //Aurora's Song | This state has been the bane of my existence, good riddance.
     // public bool Discharged;
+
+
 }
